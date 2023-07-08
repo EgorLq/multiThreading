80c6cb4fe7eafbde631045ee2a7e7081d4fd1819
Egor <52930332+EgorLq@users.noreply.github.com>
2023-07-08 15:50:13 +0300

    add final fields and codestyle

--- src/main/java/org/example/CharacterRunnable.java
+++ src/main/java/org/example/CharacterRunnable.java
@@ -3,35 +3,35 @@ package org.example;
 import java.util.concurrent.Semaphore;
 
 public class CharacterRunnable implements Runnable {
-    private String character;
-    private Scene scene;
-    private Semaphore semaphore;
+  private final String character;
+  private final Scene scene;
+  private final Semaphore semaphore;
 
-    public CharacterRunnable(String character, Scene scene, Semaphore semaphore) {
-        this.character = character;
-        this.scene = scene;
-        this.semaphore = semaphore;
-    }
+  public CharacterRunnable(String character, Scene scene, Semaphore semaphore) {
+    this.character = character;
+    this.scene = scene;
+    this.semaphore = semaphore;
+  }
 
-    @Override
-    public void run() {
-        for (String[] dialogue : scene.getDialogues(character)) {
-            String phrase = dialogue[1];
+  @Override
+  public void run() {
+    for (String[] dialogue : scene.getDialogues(character)) {
+      String phrase = dialogue[1];
 
-            try {
-                semaphore.acquire(); // Захватить семафор перед доступом к сцене
-                while (!scene.isCharacterTurn(character)) {
-                    semaphore.release(); // Временно освободить семафор, если не наступила очередь персонажа
-                    Thread.yield(); // Уступить другим потокам возможность захватить семафор
-                    semaphore.acquire(); // Снова захватить семафор перед доступом к сцене
-                }
-                System.out.println(character + ": " + phrase);
-                scene.moveToNextCharacter(); // Перейти к очереди следующего персонажа
-                semaphore.release(); // Освободить семафор после печати реплики
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+      try {
+        semaphore.acquire(); // Захватить семафор перед доступом к сцене
+        while (!scene.isCharacterTurn(character)) {
+          semaphore.release(); // Временно освободить семафор, если не наступила очередь персонажа
+          Thread.yield(); // Уступить другим потокам возможность захватить семафор
+          semaphore.acquire(); // Снова захватить семафор перед доступом к сцене
         }
+        System.out.println(character + ": " + phrase);
+        scene.moveToNextCharacter(); // Перейти к очереди следующего персонажа
+        semaphore.release(); // Освободить семафор после печати реплики
+        Thread.sleep(200);
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
     }
+  }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,23 +3,22 @@ package org.example;
 import java.util.concurrent.Semaphore;
 
 public class Main {
-    public static void main(String[] args) {
-        Scene scene = new Scene();
-        Semaphore semaphore = new Semaphore(1); // Семафор для управления выполнением потоков
+  public static void main(String[] args) {
+    Scene scene = new Scene();
+    Semaphore semaphore = new Semaphore(1); // Семафор для управления выполнением потоков
 
-        Thread joeyThread = new Thread(new CharacterRunnable("Joey", scene, semaphore));
-        Thread chandlerThread = new Thread(new CharacterRunnable("Chandler", scene, semaphore));
-        Thread phoebeThread = new Thread(new CharacterRunnable("Phoebe", scene, semaphore));
-        Thread monicaThread = new Thread(new CharacterRunnable("Monica", scene, semaphore));
-        Thread rachelThread = new Thread(new CharacterRunnable("Rachel", scene, semaphore));
-        Thread rossThread = new Thread(new CharacterRunnable("Ross", scene, semaphore));
+    Thread joeyThread = new Thread(new CharacterRunnable("Joey", scene, semaphore));
+    Thread chandlerThread = new Thread(new CharacterRunnable("Chandler", scene, semaphore));
+    Thread phoebeThread = new Thread(new CharacterRunnable("Phoebe", scene, semaphore));
+    Thread monicaThread = new Thread(new CharacterRunnable("Monica", scene, semaphore));
+    Thread rachelThread = new Thread(new CharacterRunnable("Rachel", scene, semaphore));
+    Thread rossThread = new Thread(new CharacterRunnable("Ross", scene, semaphore));
 
-        joeyThread.start();
-        chandlerThread.start();
-        phoebeThread.start();
-        monicaThread.start();
-        rachelThread.start();
-        rossThread.start();
-    }
+    joeyThread.start();
+    chandlerThread.start();
+    phoebeThread.start();
+    monicaThread.start();
+    rachelThread.start();
+    rossThread.start();
+  }
 }
-
